bce1b1bc2aa218adb00c39ce969793e517d887c0
Sarah Piot <sfg.piot@outlook.com>
2020-02-22 18:24:18 -0600

    added app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <h2>Hello There</h2>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route/>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
