4eb26aa5af29d9889d4c9ef368ac705d7b5bc96f
Sarah Piot <sfg.piot@outlook.com>
2020-02-24 13:43:05 -0600

    Removed unused import.

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import history from "../utilities/history";
 
 function Rooms() {
   const [rooms, setRooms] = useState([]);
